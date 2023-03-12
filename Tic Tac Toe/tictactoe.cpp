ff15cdf8b22885f42c21918df5bad30b8de24709
genecarlino <carlino.eu@northeastern.edu>
2023-03-12 19:29:15 -0400

    tictactoe is running
    need to fix the way the game is printing the board

--- Tic Tac Toe/tictactoe.cpp
+++ Tic Tac Toe/tictactoe.cpp
@@ -1,25 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int board[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+char board[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 int row;
 int column;
-char token = 'x'
-
+bool tie = false;
+char token = 'x';
+string n1 = "";
+string n2 = "";
 
 void functionOne(){
-    
-
-    string n1;
-    string n2;
-    cout<<"enter the name the first player : \n";
-    getline(cin,n1);
-    cout<<"Enter the name of the second player : \n";
-    getline(cin,n2);
-    cout<<n1<< " is player one so they will play first \n";
-    cout<<n2<< " is player one so they will play second \n";
-
-
     cout<<"tt     |     |     \n";
     cout<<"tt  "<<board[0][0]<<"  | "<<board[0][1]<<"   |  "<<board[0][2]<<" \n";
     cout<<"tt_____|_____|_____\n";
@@ -31,70 +21,147 @@ void functionOne(){
     cout<<"tt     |     |     \n";
 }
 
-void functionTwo{
-
+void functionTwo(){
     int digit;
 
     if(token=='x')
     {
-        cout<<n1<<"please enter ";
+        cout<<n1<<" please enter ";
         cin>>digit;
-
     }
     if(token=='0')
     {
-        cout<<n2<<"please enter ";
+        cout<<n2<<" please enter ";
         cin>>digit;
     }
 
     if(digit==1)
     {
-        row=0
-        column=0
+        row=0;
+        column=0;
     }
     if(digit==2)
     {
-        row=0
-        column=1
+        row=0;
+        column=1;
     }
     if(digit==3)
     {
-        row=0
-        column=2
+        row=0;
+        column=2;
     }
     if(digit==4)
     {
-        row=1
-        column=0
+        row=1;
+        column=0;
     }
     if(digit==5)
     {
-        row=1
-        column=1
+        row=1;
+        column=1;
     }
     if(digit==6)
     {
-        row=1
-        column=2
+        row=1;
+        column=2;
     }
     if(digit==7)
     {
-        row=2
-        column=0
+        row=2;
+        column=0;
     }
     if(digit==8)
     {
-        row=2
-        column=1
+        row=2;
+        column=1;
     }
     if(digit==9)
     {
-        row=2
-        column=2
+        row=2;
+        column=2;
+    }    
+    else if(digit<1 || digit >9){
+        cout<<"Invalid !!"<<endl;
     }
 
-
-
-
-
+    //checking if the space is open if it's x's turn
+    if(token=='x' && board[row][column] != 'x' && board[row][column] != '0'){
+        board[row][column]='x';
+        token='0';
+    }
+    //if it's 0's turn and the space isn't taken 
+    else if (token=='0' && board[row][column] != 'x' && board[row][column] != '0'){
+        board[row][column]='0';
+        token='x';
+    }
+    else{
+        cout<<"There is no empty space!"<<endl;
+        functionTwo();
+    }
+    functionOne();
 }
+
+bool functionThree()
+{
+    for(int i = 0; i < 3; i++)
+    {
+        //checking the horizontal and vertical lines 
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+        {
+            return true;
+        }
+    }
+    
+    //checking the diagonal lines
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2] || board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    {
+        return true;
+    }
+
+    //checking for tie
+    for (int i=0; i< 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[i][j] != 'x' && board[i][j] != '0')
+            {
+                return false;
+            }
+        }
+    }
+    tie = true;
+    return false;
+}
+
+int main()
+{
+    cout<<"Enter the name of the first player: ";
+    getline(cin,n1);
+    cout<<"Enter the name of the second player: ";
+    getline(cin,n2);
+
+    cout<<n1<<" is player one so they will play first.\n";
+    cout<<n2<<" is player two so they will play second.\n";
+
+    while (!functionThree())
+    {
+        functionOne();
+        functionTwo();
+    }
+
+    if(token == 'x' && tie == false)
+    {
+        cout<<n2<<" Wins!\n";
+    }
+    else if(token == '0' && tie == false)
+    {
+        cout<<n1<<" Wins!\n";
+    }
+    else
+    {
+        cout<<"It's a tie!\n";
+    }
+    return 0;
+}
+
+

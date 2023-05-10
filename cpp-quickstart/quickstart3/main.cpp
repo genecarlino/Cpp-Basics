a218e003f88a89e70f80f420b0ca2c012c00d627
genecarlino <carlino.eu@northeastern.edu>
2023-05-09 21:43:50 -0400

    main.cpp works

--- cpp-quickstart/quickstart3/main.cpp
+++ cpp-quickstart/quickstart3/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <fstream> //File I/O operations
+#include <string> // std::string
 
 int main(int argc, char* argv[]){
 
-    std::cout << "argc: " << argc << std::endl;
-    for(int i=0; i < argc; i++){
-        std::cout << '\t' <<argc[i] << std::endl;
+    //std::cout << "argc: " << argc << std::endl;
+    if(argc < 2){
+        std::cout << "Please specify a file name"
+                  << std::endl
+                  << "e.g. ./prog filename.txt"
+                  << std::endl;
+        return 0;
+    }
+    std::ifstream inputFile;
+    inputFile.open(argv[1]);
+    if(inputFile.is_open()){
+        std::string line;
+        std::getline(inputFile, line);
+        std::cout <<line;
+        std::getline(inputFile, line);
+        std::cout <<line;
+       
+
+    inputFile.close();
+
+
+    return 0;
     }
 
     // std::ofstream outFile;

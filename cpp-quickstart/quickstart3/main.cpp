d46f668f0a0e1dfb66d1b7622d455f81ef477091
genecarlino <carlino.eu@northeastern.edu>
2023-05-09 21:16:38 -0400

    reorganize

--- cpp-quickstart/quickstart3/main.cpp
+++ cpp-quickstart/quickstart3/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <fstream> //File I/O operations
 
-int main(){
+int main(int argc, char* argv[]){
 
-    std::ofstream outFile;
-    outFile.open("myTextFile.txt");
+    std::cout << "argc: " << argc << std::endl;
+    for(int i=0; i < argc; i++){
+        std::cout << '\t' <<argc[i] << std::endl;
+    }
 
-    outFile << "Hello World!" << std::endl;
-    outFile << "How are you doing today?" << std::endl;
+    // std::ofstream outFile;
+    // outFile.open("myTextFile.txt");
 
-    outFile.close();
+    // outFile << "Hello World!" << std::endl;
+    // outFile << "How are you doing today?" << std::endl;
+
+    // outFile.close();
 
     return 0;
 }

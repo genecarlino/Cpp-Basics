eaac84480d6c3ddc111b78a1c1063a3a15eba01b
genecarlino <carlino.eu@northeastern.edu>
2023-05-10 10:04:04 -0400

    created setValuePassingInAPointer

---
+++ cpp-quickstart/quickstart4/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void SetValue(int& param){
+    param = 9999;
+    std::cout << "&alias: " << &(param) << std::endl;
+}
+
+void SetValuePassingInAPointer(int* p){
+    *p = 123456;
+}
+
+int main(){
+
+    int x = 10;
+    std::cout << "&x :" << &x << std::endl; 
+
+
+    SetValue(x);
+    //store the memory location of 'x' in 'ptr'
+    int* p_x = &x;
+    
+    SetValue(x);
+    std::cout << "SetValue(x): " << x << std::endl;
+    SetValuePassingInAPointer(&x);
+    std::cout << "SetValuePassingInAPointer(&x): " << x << std::endl;
+    return 0;
+}

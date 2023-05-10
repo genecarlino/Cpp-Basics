f2d95d72fb6dd925c68e15790d1bd6b53c9e4e34
genecarlino <carlino.eu@northeastern.edu>
2023-05-10 07:49:52 -0400

    more changes

--- cpp-quickstart/quickstart3/main.cpp
+++ cpp-quickstart/quickstart3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream> //File I/O operations
 #include <string> // std::string
+#include <sstream> // std::stringstream
 
 int main(int argc, char* argv[]){
 
@@ -15,18 +16,27 @@ int main(int argc, char* argv[]){
     std::ifstream inputFile;
     inputFile.open(argv[1]);
     if(inputFile.is_open()){
+        //Temp storage for each line
         std::string line;
-        std::getline(inputFile, line);
-        std::cout <<line;
-        std::getline(inputFile, line);
-        std::cout <<line;
-       
-
+        //while loop to read in the data
+        while(std::getline(inputFile, line)){
+            //question why 0? it effects more than line 0
+            if(line[0] == '#'){
+                //do nothing
+            }else{
+            //construct a string stream from out line
+            std::stringstream stream (line);
+            //break out each individual component
+            //based off of spaces
+            std::cout <<line<<std::endl;
+            }
+        }
+        
+    }
     inputFile.close();
 
 
     return 0;
-    }
 
     // std::ofstream outFile;
     // outFile.open("myTextFile.txt");
